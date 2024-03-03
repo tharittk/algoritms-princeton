fb2111ea99df3b4aa4a7b2d32a3a44c22e6fa9e1
Tharit Tangkijwanichakul <tharitt@pttep.com>
2024-03-03 15:37:49 +0700

    starter

---
+++ queues/RandomizedQueue.java
@@ -0,0 +1,51 @@
+/* *****************************************************************************
+ *  Name: Tharit T.
+ *  Date: March 3, 2024
+ *  Description:
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return 0;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        return;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        return;
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
