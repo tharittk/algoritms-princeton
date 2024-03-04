7efa0d8b1a2e7f026bf0dd0ec5571ce1a5d5f29f
Tharit Tangkijwanichakul <tharitt@pttep.com>
2024-03-04 22:26:40 +0700

    permutation class

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -4,8 +4,24 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        String s;
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()) {
+            s = StdIn.readString();
+            rq.enqueue(s);
+        }
+
+        for (int i = 0; i < n; i++) {
+            s = rq.dequeue();
+            StdOut.println(s);
+        }
+
 
     }
 }
