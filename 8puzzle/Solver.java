6085bafedef9aa2afa43b742ac02dd8242a13b9c
Tharit Tangkijwanichakul <tharitt@pttep.com>
2024-03-17 11:23:44 +0700

    working solution tested without twins

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -4,8 +4,11 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 
@@ -89,26 +92,57 @@ public class Solver {
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
         // not yet implement twins
-        return terminalSearch.moveSoFar;
+        if (!this.isSolvable()) return -1;
+        else return terminalSearch.moveSoFar;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+
+        if (!this.isSolvable()) return null;
+
         SearchNode currentSearch;
-        Stack<Board> solutionPath = new Stack<Board>();
+        Stack<Board> solutionStack = new Stack<Board>();
+        Queue<Board> solutionPath = new Queue<Board>();
+
         currentSearch = terminalSearch;
         while (currentSearch.prevSearch != null) {
-            solutionPath.push(currentSearch.associatedBoard);
+            solutionStack.push(currentSearch.associatedBoard);
             currentSearch = currentSearch.prevSearch;
         }
         // push to the root search node
-        solutionPath.push(rootSearch.associatedBoard);
+        solutionStack.push(rootSearch.associatedBoard);
+
+        while (!solutionStack.isEmpty()) {
+            solutionPath.enqueue(solutionStack.pop());
+        }
+
         return solutionPath;
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        System.out.println(">> Start solver");
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
 
 }
