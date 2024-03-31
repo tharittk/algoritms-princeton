0e17c8329fdc4497686df36865d4c51b5a2cfd02
Tharit Tangkijwanichakul <tharitt@pttep.com>
2024-03-31 21:08:39 +0700

    Brute force done & test

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -1,7 +1,7 @@
 /* *****************************************************************************
  *  Name: Tharit T.
  *  Date: March 31.
- *  Description: Find all points that is inside quried rectangle and the nearest point to the queried point.
+ *  Description: Find all points that is inside queried rectangle and the nearest point to the queried point.
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.In;
@@ -9,6 +9,7 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class PointSET {
@@ -48,13 +49,20 @@ public class PointSET {
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return points;
-
+        ArrayList<Point2D> pointInside = new ArrayList<Point2D>();
+        for (Point2D p : points) {
+            if (rect.contains(p)) pointInside.add(p);
+        }
+        return pointInside;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return points.last();
+        Point2D champion = points.first();
+        for (Point2D thatPoint : points) {
+            if (p.distanceTo(thatPoint) < p.distanceTo(champion)) champion = thatPoint;
+        }
+        return champion;
     }
 
     // unit testing of the methods (optional)
@@ -73,12 +81,34 @@ public class PointSET {
         }
 
         in.close();
-
         // draw the points
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 1);
         StdDraw.setYscale(0, 1);
-        pointSet.draw();
+        // pointSet.draw();
+
+        // Set-up rectangle
+        double xmin, xmax, ymin, ymax;
+        xmin = 0.3;
+        xmax = 0.7;
+        ymin = 0.4;
+        ymax = 0.6;
+        RectHV rect = new RectHV(xmin, ymin, xmax, ymax);
+        rect.draw();
         StdDraw.show();
+
+        // Run range algorithms
+        Iterable<Point2D> pointInside = pointSet.range(rect);
+
+        for (Point2D p : pointInside) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        Point2D nearTestPoint = new Point2D(0.9, 0.9);
+        Point2D nearestPoint = pointSet.nearest(nearTestPoint);
+
+        System.out.println("Nearest Point: " + nearestPoint);
+
     }
 }
