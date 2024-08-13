d8aa80d03ab7946255984b576be7dff7aaf0af56
TharitT <tharit.tangkij@gmail.com>
2024-08-13 21:57:33 +0700

    Customized Trie for Prefix validation- Blazingly Fast

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -1,6 +1,5 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TST;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -8,10 +7,10 @@ import java.util.HashSet;
 public class BoggleSolver {
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-    private TST<Integer> tries;
+    private CustomTST<Integer> tries;
 
     public BoggleSolver(String[] dictionary) {
-        tries = new TST<Integer>();
+        tries = new CustomTST<Integer>();
         for (String s : dictionary) {
             tries.put(s, s.length());
         }
@@ -56,7 +55,7 @@ public class BoggleSolver {
         if (s.length() <= 2) { // down more
             doDFS = true;
         }
-        else if (this.tries.keysWithPrefix(s) != null) { // length > 2
+        else if (this.tries.isPrefixOfAnyWord(s)) { // length > 2
             acc.add(s);
             doDFS = true;
         }
@@ -141,6 +140,126 @@ public class BoggleSolver {
             score += solver.scoreOf(word);
         }
         StdOut.println("Score = " + score);
+    }
+
+    // Customized Trie - https://algs4.cs.princeton.edu/52trie/TST.java.html
+    private class CustomTST<Value> {
+        private int n;              // size
+        private Node<Value> root;   // root of TST
+
+        private class Node<Value> {
+            private char c;                        // character
+            private Node<Value> left, mid, right;  // left, middle, and right subtries
+            private Value val;                     // value associated with string
+        }
+
+        /**
+         * Initializes an empty string symbol table.
+         */
+        public CustomTST() {
+        }
+
+
+        /**
+         * Does this symbol table contain the given key?
+         *
+         * @param key the key
+         * @return {@code true} if this symbol table contains {@code key} and
+         * {@code false} otherwise
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public boolean contains(String key) {
+            if (key == null) {
+                throw new IllegalArgumentException("argument to contains() is null");
+            }
+            return get(key) != null;
+        }
+
+        /**
+         * Returns the value associated with the given key.
+         *
+         * @param key the key
+         * @return the value associated with the given key if the key is in the symbol table
+         * and {@code null} if the key is not in the symbol table
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public Value get(String key) {
+            if (key == null) {
+                throw new IllegalArgumentException("calls get() with null argument");
+            }
+            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+            Node<Value> x = get(root, key, 0);
+            if (x == null) return null;
+            return x.val;
+        }
+
+        // return subtrie corresponding to given key
+        private Node<Value> get(Node<Value> x, String key, int d) {
+            if (x == null) return null;
+            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+            char c = key.charAt(d);
+            if (c < x.c) return get(x.left, key, d);
+            else if (c > x.c) return get(x.right, key, d);
+            else if (d < key.length() - 1) return get(x.mid, key, d + 1);
+            else return x;
+        }
+
+        /**
+         * Inserts the key-value pair into the symbol table, overwriting the old value
+         * with the new value if the key is already in the symbol table.
+         * If the value is {@code null}, this effectively deletes the key from the symbol table.
+         *
+         * @param key the key
+         * @param val the value
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public void put(String key, Value val) {
+            if (key == null) {
+                throw new IllegalArgumentException("calls put() with null key");
+            }
+            if (!contains(key)) n++;
+            else if (val == null) n--;       // delete existing key
+            root = put(root, key, val, 0);
+        }
+
+        private Node<Value> put(Node<Value> x, String key, Value val, int d) {
+            char c = key.charAt(d);
+            if (x == null) {
+                x = new Node<Value>();
+                x.c = c;
+            }
+            if (c < x.c) x.left = put(x.left, key, val, d);
+            else if (c > x.c) x.right = put(x.right, key, val, d);
+            else if (d < key.length() - 1) x.mid = put(x.mid, key, val, d + 1);
+            else x.val = val;
+            return x;
+        }
+
+
+        // check if the given prefix is a prefix of any word in trie
+        private boolean isPrefixOfAnyWord(String prefix) {
+            if (prefix == null) {
+                throw new IllegalArgumentException("calls isPrefixOfAnyWord() with null argument");
+            }
+            if (prefix.length() == 0)
+                throw new IllegalArgumentException("prefix must have length >= 1");
+            Node<Value> x = isPrefixOfAnyWord(root, prefix, 0);
+            if (x == null) return false;
+            return true;
+        }
+
+        // check if the given prefix is a prefix of any word in trie
+        private Node<Value> isPrefixOfAnyWord(Node<Value> x, String prefix, int d) {
+            if (x == null) return null;
+            if (prefix.length() == 0)
+                throw new IllegalArgumentException("prefix must have length >= 1");
+            char c = prefix.charAt(d);
+            if (c < x.c) return isPrefixOfAnyWord(x.left, prefix, d);
+            else if (c > x.c) return isPrefixOfAnyWord(x.right, prefix, d);
+            else if (d < prefix.length() - 1) return get(x.mid, prefix, d + 1);
+            else return x;
+        }
+
     }
 
 }
