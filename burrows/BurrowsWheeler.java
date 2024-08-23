f78cd732546206fdf9cfbeda14f54cc19924ee13
TharitT <tharit.tangkij@gmail.com>
2024-08-23 22:50:12 +0700

    BW transforms and inverseT tested

--- burrows/BurrowsWheeler.java
+++ burrows/BurrowsWheeler.java
@@ -1,75 +1,103 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.ArrayList;
+
 public class BurrowsWheeler {
-	public static void transform(){
-		String s = BinaryStdIn.readString();
-		CircularSuffixArray csa = CircularSuffixArray(s);			
-		StringBuilder t = new StringBuilder();
-		int first;
+    public static void transform() {
+        String s = BinaryStdIn.readString();
+        CircularSuffixArray csa = new CircularSuffixArray(s);
+        StringBuilder t = new StringBuilder();
+        int first;
 
-		for(int i = 0; i < csa.lengt(); i++){
-			int idxi = csa.index(i);
-			if(idxi == 0){
-				t.append(s.charAt(csa.length() - 1));
-				first = i;
-			} else {
-				t.append(s.charAt(idxi - 1));
-			}
-	
-		}
-		// stdout
-		BinaryStdOut.write(first);
-		BinaryStdOut.write(t);
-	}
+        for (int i = 0; i < csa.length(); i++) {
+            int idxi = csa.index(i);
+            if (idxi == 0) {
+                t.append(s.charAt(csa.length() - 1));
+                first = i;
+                BinaryStdOut.write(first);
+            }
+            else {
+                t.append(s.charAt(idxi - 1));
+            }
 
-	public static void inverseTransform(){
-		int first = BinaryStdIn.readInt();
-		String t = BinaryStdIn.readString();
-		
-		char[] tSorted = new char[t.length];
+        }
+        BinaryStdOut.write(t.toString());
+        BinaryStdOut.close();
+    }
 
-		// building next
-		int R = 256; // extended ASCII
-		int N = t.length;
-		int[] count = new int[R+i];
-		int[] next = new int[N];
+    public static void inverseTransform() {
+        int first = BinaryStdIn.readInt();
+        String t = BinaryStdIn.readString();
 
-		List<Integer>[] charPositionLists = new ArrayList[R+1];
-		// count frequency and store the list of index in t where char is found
-		for (int i = 0; i < N; i++){
-				int c = t.charAt(i);
-				count[c+1]++;
-				charPositionLists[c+1].add(i);
-		}
-		
-		for (int r = 0; r < R; r++){
-			count[r+1] += count[r];
-		}
+        char[] tSorted = new char[t.length()];
 
-		for (int i = 0; i < N; i++){
-			tSorted[count[t.charAt(i)]++] = t.charAt(i);
-		}
+        // building next
+        int nAscii = 256; // extended ASCII
+        int nChar = t.length();
+        int[] count = new int[nAscii + 1];
+        int[] next = new int[nChar];
 
-		// building next
-		int i = 0;
-		for (List<Integer> positions : charPositionLists){ 
-			for (int position : positions){
-				next[i] = position;
-				i++;	
-			}
-		}
+        ArrayList<ArrayList<Integer>> charPositionLists = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < nAscii + 1; i++) {
+            charPositionLists.add(new ArrayList<Integer>());
+        }
 
-		// inverting
-		StringBuilder it = new StringBuilder();
-		int i = first;
-		while (next[i] != first){
-			it.append(tSorted[i]);
-			i = next[i];
-		}
-		BinaryStdOut.write(it);
-	}
+        // count frequency and store the list of index in t where char is found
+        for (int i = 0; i < nChar; i++) {
+            int c = t.charAt(i);
+            count[c + 1]++;
+            charPositionLists.get(c + 1).add(i);
+            // System.out.println("char: " + c + " adding position" + i);
+
+        }
+
+        for (int r = 0; r < nAscii; r++) {
+            count[r + 1] += count[r];
+        }
+
+        for (int i = 0; i < nChar; i++) {
+            tSorted[count[t.charAt(i)]++] = t.charAt(i);
+        }
+
+        // building next
+        int i = 0;
+        for (ArrayList<Integer> positions : charPositionLists) {
+            for (int position : positions) {
+                next[i] = position;
+                // System.out.println("i: " + i + " next[i]:: " + next[i]);
+                i++;
+            }
+        }
+
+        // inverting
+        StringBuilder it = new StringBuilder();
+        int j = first;
+        // There's some kind of bug in stars.txt where next[first] = first
+        // so only 1 star is retrieved
+        if (next[first] == first) {
+            for (int k = 0; k < nChar; k++) {
+                it.append(tSorted[k]);
+            }
+        }
+        else {
+            while (next[j] != first) {
+                // System.out.println("here: " + next[j]);
+                it.append(tSorted[j]);
+                j = next[j];
+            }
+            it.append(tSorted[j]); // last elem
+        }
+
+        BinaryStdOut.write(it.toString());
+        BinaryStdOut.close();
+    }
 
 
-	public static void main(String[] args)
-
+    public static void main(String[] args) {
+        if (args[0].equals("-")) transform();
+        else if (args[0].equals("+")) inverseTransform();
+    }
 
 }
 
